5e87d854b10048913bd9b49f261020a3c59bf028
Jonathan Valderrama <jvalderrama91@knights.ucf.edu>
2019-05-30 16:48:39 -0400

    section 8 finished

--- RobotGuardBTStarter/Assets/AI.cs
+++ RobotGuardBTStarter/Assets/AI.cs
@@ -58,6 +58,14 @@ public class AI : MonoBehaviour
     }
 
     [Task]
+    public void PickDestination(float x, float z)
+    {
+        Vector3 dest = new Vector3(x, 0, z);
+        agent.SetDestination(dest);
+        Task.current.Succeed();
+    }
+
+    [Task]
     public void MoveToDestination()
     {
         if (Task.isInspected)
@@ -68,5 +76,121 @@ public class AI : MonoBehaviour
         }
     }
 
+    [Task]
+    public void TargetPlayer()
+    {
+        target = player.transform.position;
+        Task.current.Succeed();
+    }
+
+    [Task]
+    bool Turn(float angle)
+    {
+        var p = this.transform.position + Quaternion.AngleAxis(angle, Vector3.up) * this.transform.forward;
+        target = p;
+        return true;
+    }
+
+    [Task]
+    public void LookAtTarget()
+    {
+        Vector3 direction = target - this.transform.position;
+
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+        Debug.Log(Vector3.Angle(this.transform.forward, direction));
+
+
+        if (Task.isInspected)
+            Task.current.debugInfo = string.Format("angle={0}", 
+                                                   Vector3.Angle(this.transform.forward,direction));
+
+        if (Vector3.Angle(this.transform.forward, direction) < 5.0f)
+        {
+            Task.current.Succeed();
+        }
+    }
+
+    [Task]
+    public bool Fire()
+    {
+        GameObject bullet = GameObject.Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * 2000);
+        return true;
+    }
+
+    [Task]
+    bool SeePlayer()
+    {
+        Vector3 distance = player.transform.position - this.transform.position;
+
+        RaycastHit hit;
+        bool seeWall = false;
+
+        Debug.DrawRay(this.transform.position, distance, Color.red);
+
+        if(Physics.Raycast(this.transform.position,distance, out hit))
+        {
+            if(hit.collider.gameObject.tag == "wall")
+            {
+                seeWall = true;
+            }
+        }
+
+        if (Task.isInspected)
+            Task.current.debugInfo = string.Format("wall={0}", seeWall);
+
+        if (distance.magnitude < visibleRange && !seeWall)
+            return true;
+        else
+            return false;
+    }
+
+    [Task]
+    public bool IsHealthLessThan(float health)
+    {
+        return this.health < health;
+    }
+
+    [Task]
+    public bool InDanger(float minDist)
+    {
+        Vector3 distance = player.transform.position - this.transform.position;
+        return (distance.magnitude < minDist);
+    }
+
+    [Task]
+    public void TakeCover()
+    {
+        Vector3 awayFromPlayer = this.transform.position - player.transform.position;
+        Vector3 dest = this.transform.position + awayFromPlayer * 2;
+        agent.SetDestination(dest);
+        Task.current.Succeed();
+    }
+
+    [Task]
+    public bool Explode()
+    {
+        Destroy(healthBar.gameObject);
+        Destroy(this.gameObject);
+        return true;
+    }
+
+    [Task]
+    public void SetTargetDestination()
+    {
+        agent.SetDestination(target);
+        Task.current.Succeed();
+    }
+
+    [Task]
+    bool ShotLinedUp()
+    {
+        Vector3 distance = target - this.transform.position;
+        if (distance.magnitude < shotRange &&
+                Vector3.Angle(this.transform.forward, distance) < 1.0f)
+            return true;
+        else
+            return false;
+    }
 }
 
